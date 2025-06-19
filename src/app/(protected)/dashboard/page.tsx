5dcb9618d9700abfb4fced78f7f609742d5b0030
Bassam-Elsaied <bassame993@gmail.com>
2025-06-19 15:02:43 +0300

    front page

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,5 +1,17 @@
+import RecentProperty from "@/components/Dashboard/RealEstate/RecentProperty";
+import Welcome from "@/components/Dashboard/Restaurant/Welcome";
+
 export const dynamic = "force-dynamic";
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <>
+      <Welcome />
+      <div className="2xl:grid 2xl:grid-cols-2 gap-[25px]">
+        <div>
+          <RecentProperty />
+        </div>
+      </div>
+    </>
+  );
 }
